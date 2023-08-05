d4a3ba3f80de805b5886eafda90194e47cc1cb0e
Milton Salustiano <109237764+miltonneto10@users.noreply.github.com>
2023-08-05 14:46:17 -0300

    Commit Inicial

---
+++ Operador.java
@@ -0,0 +1,20 @@
+//Operador Matemático
+
+//os operadores são símbolos utilizados na execução de operações, assim como nas operações lógicas e matemáticas, estão divididos em três grupos: Operadores Matemáticos, de Incremento e Lógicos
+public class Operador {
+	//Método principal da classe
+	public static void main (String [] args) {
+		double x = 7 + 3
+		System.out.println("x");
+	}//fim do método
+}//fim da classe
+
+/** double faz parte do GRUPO DE TIPOS PRIMITIVOS,  o DOUBLE é um número de ponto flutuante de 64 bits(signed)
+
+Há situações em que você precisará indicar se um número é positivo ou negativo. Para tanto, é possível informar que seu número é positivo ou negativo simplesmente adicionando os respectivos sinais de mais (+) ou menos (-), seguidos do número.
+Por exemplo:
+
+						double x = +3
+						double x = -3
+
+**/
